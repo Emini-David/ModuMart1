730798ae675ca150171cd9aa92f8fa6a2d9f9b5c
Emini-David <davidayodele377@gmail.com>
2025-10-01 16:15:25 +0100

    changes made

---
+++ ModuMart/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './component/navbar/navbar.jsx'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
